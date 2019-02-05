6eb3c4f61f05d9dda0057b78fb730aad97eed89b
German Laullon <laullon@gmail.com>
2019-02-05 19:43:09 +0100

    Derived Metrics (#3)
    * initial commit
    
    * use correct histogram
    
    * Update .travis.yml
    
    * changes on HeartbeatService
    
    * update dep
    
    * RateSampler, update deps and final touches

--- reporter/reporter.go
+++ reporter/reporter.go
@@ -1,40 +1,67 @@
 package reporter
 
 import (
+	"fmt"
 	"os"
+	"strings"
 
+	"github.com/opentracing/opentracing-go/ext"
+	metrics "github.com/rcrowley/go-metrics"
+	"github.com/wavefronthq/go-metrics-wavefront/reporting"
 	"github.com/wavefronthq/wavefront-opentracing-sdk-go/tracer"
 	"github.com/wavefronthq/wavefront-sdk-go/application"
-	wf "github.com/wavefronthq/wavefront-sdk-go/senders"
+	"github.com/wavefronthq/wavefront-sdk-go/senders"
 )
 
 // WavefrontSpanReporter implements the wavefront.Reporter interface.
-type WavefrontSpanReporter struct {
+type WavefrontSpanReporter interface {
+	tracer.SpanReporter
+	Flush()
+}
+
+type reporter struct {
 	source      string
-	sender      wf.Sender
+	sender      senders.Sender
 	application application.Tags
+	metrics     reporting.WavefrontMetricsReporter
+	heartbeater application.HeartbeatService
 }
 
 // Option allow WavefrontSpanReporter customization
-type Option func(*WavefrontSpanReporter)
+type Option func(*reporter)
 
 // Source tag for the spans
 func Source(source string) Option {
-	return func(args *WavefrontSpanReporter) {
+	return func(args *reporter) {
 		args.source = source
 	}
 }
 
 // New returns a WavefrontSpanReporter for the given `sender`.
-func New(sender wf.Sender, application application.Tags, setters ...Option) *WavefrontSpanReporter {
-	r := &WavefrontSpanReporter{
+func New(sender senders.Sender, app application.Tags, setters ...Option) WavefrontSpanReporter {
+	r := &reporter{
 		sender:      sender,
 		source:      hostname(),
-		application: application,
+		application: app,
 	}
+
 	for _, setter := range setters {
 		setter(r)
 	}
+
+	r.metrics = reporting.NewReporter(
+		sender,
+		r.application,
+		reporting.Source(r.source),
+		reporting.Prefix("tracing.derived"),
+	)
+
+	r.heartbeater = application.StartHeartbeatService(
+		sender,
+		r.application,
+		r.source,
+	)
+
 	return r
 }
 
@@ -47,7 +74,8 @@ func hostname() string {
 }
 
 // ReportSpan complies with the tracer.Reporter interface.
-func (t *WavefrontSpanReporter) ReportSpan(span tracer.RawSpan) {
+func (t *reporter) ReportSpan(span tracer.RawSpan) {
+	t.reportDerivedMetrics(span)
 	if !span.Context.Sampled {
 		return
 	}
@@ -56,9 +84,54 @@ func (t *WavefrontSpanReporter) ReportSpan(span tracer.RawSpan) {
 	parents, followsFrom := prepareReferences(span)
 
 	for k, v := range t.application.Map() {
-		tags = append(tags, wf.SpanTag{Key: k, Value: v})
+		tags = append(tags, senders.SpanTag{Key: k, Value: v})
 	}
 
 	t.sender.SendSpan(span.Operation, span.Start.UnixNano()/1000000, span.Duration.Nanoseconds()/1000000, t.source,
 		span.Context.TraceID, span.Context.SpanID, parents, followsFrom, tags, nil)
+}
+
+func (t *reporter) reportDerivedMetrics(span tracer.RawSpan) {
+	metricName := fmt.Sprintf("%s.%s.%s", t.application.Application, t.application.Service, span.Operation)
+	metricName = strings.Replace(metricName, " ", "-", -1)
+	metricName = strings.Replace(metricName, "\"", "\\\"", -1)
+
+	tags := t.application.Map()
+	tags["operationName"] = span.Operation
+
+	t.getHistogram(metricName+".duration.micros", tags).Update(span.Duration.Nanoseconds() / 1000)
+	t.getCounter(metricName+".total_time.millis", tags).Inc(span.Duration.Nanoseconds() / 1000000)
+	t.getCounter(metricName+".invocation", tags).Inc(1)
+	errors := t.getCounter(metricName+".error", tags)
+	if span.Tags[string(ext.Error)] == true {
+		errors.Inc(1)
+	}
+}
+
+func (t *reporter) getHistogram(name string, tags map[string]string) reporting.Histogram {
+	h := reporting.GetMetric(name, tags)
+	if h == nil {
+		h = reporting.NewHistogram()
+		err := reporting.RegisterMetric(name, h, tags)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return h.(reporting.Histogram)
+}
+
+func (t *reporter) getCounter(name string, tags map[string]string) metrics.Counter {
+	c := reporting.GetMetric(name, tags)
+	if c == nil {
+		c = metrics.NewCounter()
+		err := reporting.RegisterMetric(name, c, tags)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return c.(metrics.Counter)
+}
+
+func (t *reporter) Flush() {
+	t.metrics.Report()
 }
